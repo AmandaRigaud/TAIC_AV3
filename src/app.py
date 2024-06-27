8e895d86a276c9c4bd11562c1a8af8035f8e65bd
juliane-coimbra <62030952+JuCoimbra@users.noreply.github.com>
2024-06-27 16:00:11 -0300

    Add get route to openai media generated

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import openai
 
 app = Flask(__name__)
 
@@ -6,5 +7,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/get/<string:user_input>')
+def get_media(user_input):
+    chat_response = openai.Chat(user_input)
+    # split_response = chat_response.split('\n\n')
+
+    # audio_list = []
+    # image_list = []
+
+    # for count, paragraph in enumerate(split_response):
+    #     image_list.append(openai.Image(paragraph))
+
+    #     openai.Audio(paragraph, f'audio{count}.mp3')
+    #     audio_list.append(f'audio{count}.mp3')
+
+    img_url = openai.Image(chat_response)
+    openai.Audio(chat_response, '/static/audio/audio.mp3')
+    
+    response = {
+        'audio_url': '/static/audio/audio.mp3',
+        'image_url': img_url
+    }
+    
+    return jsonify(response)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
