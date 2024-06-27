28efe017f031a56e90e83df97da4ba55ab1203aa
juliane-coimbra <62030952+JuCoimbra@users.noreply.github.com>
2024-06-27 20:30:41 -0300

    Configure server on run app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-import openai
+import openaiAPI
 
 app = Flask(__name__)
 
@@ -9,28 +9,29 @@ def home():
 
 @app.route('/get/<string:user_input>')
 def get_media(user_input):
-    chat_response = openai.Chat(user_input)
+
+    chat_response = openaiAPI.Chat(user_input)
     # split_response = chat_response.split('\n\n')
 
     # audio_list = []
     # image_list = []
 
     # for count, paragraph in enumerate(split_response):
-    #     image_list.append(openai.Image(paragraph))
+    #     image_list.append(openaiAPI.Image(paragraph))
 
-    #     openai.Audio(paragraph, f'audio{count}.mp3')
+    #     openaiAPI.Audio(paragraph, f'audio{count}.mp3')
     #     audio_list.append(f'audio{count}.mp3')
 
-    img_url = openai.Image(chat_response)
-    openai.Audio(chat_response, '/static/audio/audio.mp3')
+    img_url = openaiAPI.Image(chat_response)
+    openaiAPI.Audio(chat_response, './static/audio/audio.mp3')
     
     response = {
-        'audio_url': '/static/audio/audio.mp3',
+        'audio': './static/audio/audio.mp3',
         'image_url': img_url
     }
     
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
